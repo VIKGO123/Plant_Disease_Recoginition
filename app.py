7a2f942e894b74d056be8a46a6fa787799bc9b14
VIKASH PATHAK <vikashpathak1121@gmail.com>
2019-12-17 12:48:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ import face_recognition
 import cv2
 import face_recognition
 from torch import nn
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+
+from PIL import Image
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+
 device = torch.device('cpu')
 
 def transform_image(image_bytes):
