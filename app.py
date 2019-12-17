f5dd3fe7d6f1b2503ed81fe7ad252c5653319462
VIKASH PATHAK <vikashpathak1121@gmail.com>
2019-12-17 13:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ import face_recognition
 from torch import nn
 import six.moves.urllib as urllib
 import sys
-import tarfile
+
 import tensorflow as tf
-import zipfile
+
 
 from collections import defaultdict
 from io import StringIO
