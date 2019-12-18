4817f070a1ed867b410a28ff9f28f6f432954210
VIKASH PATHAK <vikashpathak1121@gmail.com>
2019-12-18 11:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,8 @@ from io import StringIO
 # from matplotlib import pyplot as plt
 
 from PIL import Image
-
-# from object_detection.utils import label_map_util
-# from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
 
 device = torch.device('cpu')
